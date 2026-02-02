6ec03aa98bd12cb35241dfe9c6f8a0296efb96c1
NachtWaker2000 <225383022+NachtWaker2000@users.noreply.github.com>
2026-02-02 16:25:26 +0100

    feat: add save game function and play menu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import json
 
+active_player = None
 
 def main_menu():
     os.system('clear')
@@ -17,7 +18,7 @@ def main_menu():
 def main_menu_selection():
     choice = int(input('please enter your choice: '))
     if choice == 1:
-        play_menu()
+        login_menu()
     elif choice == 2:
         print('This has not been implemented yet')
         main_menu_selection()
@@ -30,15 +31,15 @@ def main_menu_selection():
         main_menu_selection()
 
 
-def play_menu():
+def login_menu():
     os.system('clear')
     print("#####################################")
     print("1.           Login              #####")
     print("2.     Create an account        #####")
     print("#####################################")
-    play_menu_selection()
+    login_menu_selection()
 
-def play_menu_selection():
+def login_menu_selection():
     choice = int(input('please enter your choice: '))
     if choice == 1:
         load_save()
@@ -47,10 +48,45 @@ def play_menu_selection():
     else:
         print("Invalid input")
         print("Please try again")
-        play_menu_selection()
+        login_menu_selection()
+
+def play_menu(active_player):
+    os.system('clear')
+    print("#####################################")
+    print("1.fight                      ########")
+    print("2.shop                       ########")
+    print("3.equipment                  ########")
+    print("4.save game                  ########")
+    print("5.take dmg                   ########")
+    print("#####################################")
+    print(active_player.username)
+    play_menu_selection(active_player)
+
+def play_menu_selection(active_player):
+    choice = int(input('please enter your choice: '))
+    if choice == 1:
+        pass
+    elif choice == 2:
+        pass
+    elif choice == 3:
+        pass
+    elif choice == 4:
+        save_game(active_player)
+    elif choice == 5:
+        print(active_player.username)
+        print('your current health is :',active_player.health)
+        active_player.health = active_player.health - 10
+        print('your current health is now:',active_player.health)
+        input('press enter to continue')
+        play_menu(active_player)
+    else:
+        print("Invalid input")
+        print("Please try again")
+        login_menu_selection()
 
 class player():
-    def __init__ (self, health, shield, gold):
+    def __init__ (self, username, health, shield, gold):
+        self.username = username
         self.health = health
         self.shield = shield
         self.gold = gold
@@ -58,10 +94,14 @@ class player():
     @classmethod
     def from_dict(cls, data):
         return cls(
+            username = data['username'],
             health = data['health'],
             shield = data['shield'],
             gold = data['gold']
             )
+    
+    def to_dict(self):
+        return { 'username' : self.username, 'health' : self.health, 'shield' : self.shield, 'gold' : self.gold}
 
 
 def create_account():
@@ -70,13 +110,12 @@ def create_account():
         all_saves = json.load(f)
         if name in all_saves:
             print("This name is already taken, please choose another")
-            name = input("Please enter your desired name: ")
-            create_account(name)
+            create_account()
         else:
-            new_player = player(100, 50, 0)
+            new_player = player(name,100, 50, 0)
             all_saves[name] = new_player.__dict__
             with open ('saves.json', 'w') as f:
-                json.dump(all_saves, f)
+                json.dump(all_saves, f, indent=4)
             print("Account created successfully!")
 
 
@@ -84,23 +123,34 @@ def create_account():
 def load_save():
     with open ('saves.json' , 'r') as f:
         all_saves = json.load(f)
+        print("#####################################")
+        print("########       login         ########")
+        print("########                     ########")
+        print("#####################################")
         name = input("Please enter your account name:")
         if name in all_saves:
             save_data = all_saves[name]
-            print(save_data)
             active_player = player.from_dict(save_data)
-            print(active_player)
-            print(active_player.health)
-            print(active_player.shield)
-            print(active_player.gold)
+            print(active_player.username)
+            print('Login successful !')
+            input('press enter to continue')
+            play_menu(active_player)
         else:
-            pass
+            print('Username is invalid !')
+            
         
 
-def save_game():
-    with open ('saves.json', 'w') as f:
-        pass
-
+def save_game(active_player):
+    with open ('saves.json', 'r') as f:
+        all_saves = json.load(f)
+        all_saves[active_player.username] = active_player.to_dict()
+        print('this is all_saves')
+        print(all_saves)
+        print('this is all_saves')
+        with open ('saves.json', 'w') as f:
+            json.dump(all_saves, f, indent=4)
+            print('saved successfully !')
+            input('press enter to continue')
 
 
 
