1e70118eb815d031a0b4f9215b03cf56de487d3e
NachtWaker2000 <225383022+NachtWaker2000@users.noreply.github.com>
2026-02-04 09:38:11 +0100

    feat: add enemy generation

---
+++ enemies.py
@@ -0,0 +1,8 @@
+class enemies():
+    def __init__ (self, name, exp, level, health, shield, gold):
+        self.exp = exp
+        self.level = level
+        self.name = name
+        self.health = health
+        self.shield = shield
+        self.gold = gold
--- systems.py
+++ systems.py
@@ -1,5 +1,6 @@
-
-
+from enemies import enemies
+from random import randint
+import data
 
 def end_turn(state):
     state.turn = state.turn + 1
@@ -20,7 +21,7 @@ def handle_command(state, command):
     else:
         invalid()
 
-def set_zone_travel(state):
+def set_state_travel(state):
     state.zone = 'travel_zone'
     
 
@@ -35,10 +36,44 @@ def get_input():
 def invalid():
     print('Invalid command please try again !')
 
+def fight():
+    pass
+
+def generate_enemy(state):
+    enemy_lvl = generate_enemy_lvl(state)
+    enemy_name = generate_enemy_name(state)
+    enemy_exp = BASE_EXP * (enemy_lvl / state.player.level) ^ 1.2
+
+
+def generate_enemy_name(state):
+    zone_name = ENEMY_NAMES[state.zone]
+    index= randint(0, lambda x:len(zone_name) -1)
+    return zone_name[index]
+
+def generate_enemy_lvl(state):
+    zone_lvl = ENEMY_LEVELS[state.zone]
+    index= randint(0, lambda x:len(zone_lvl) -1)
+    return zone_lvl[index]
+
+def shop():
+    pass
+
+
+#constant enemies
+
+ZONE_DATA = {'forest' : {
+             "min_lvl": 1,
+             "max_lvl": 1,
+             "zone_lvl": 1,
+             "base_exp": 1,
+             "base_gold": 1,
+             "base_health": 1,
+             "base_defense": 1,
+             "enemy_names": ['goblin','rat', 'bat']}}
 
 #constant data maps
 HUB_COMMANDS = {
-    "1": set_zone_travel,
+    "1": set_state_travel,
     "2": 'temp',
     "3": 'temp'
 }
@@ -50,7 +85,10 @@ TRAVEL_COMMANDS = {
 }
 
 FOREST_COMMANDS = {
-    "fight": 'temp',
-    "flee": 'temp',
+    "1": fight,
+    "2": shop,
+    "3": set_state_travel,
+
+
 }
 
